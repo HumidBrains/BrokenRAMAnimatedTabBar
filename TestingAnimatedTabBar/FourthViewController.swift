e53401377d1d7efb04571e7427d1bc208491e03d
humidbrains <jimmy.nordbeck@gmail.com>
2016-04-08 10:26:23 +0200

    Basic setup without AnimatedTabBar

---
+++ TestingAnimatedTabBar/FourthViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FourthViewController.swift
+//  TestingAnimatedTabBar
+//
+//  Created by Jimmy Nordbeck on 08/04/16.
+//  Copyright © 2016 Jimmy Nordbeck. All rights reserved.
+//
+
+import UIKit
+
+class FourthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TestingAnimatedTabBar/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TestingAnimatedTabBar
+//
+//  Created by Jimmy Nordbeck on 08/04/16.
+//  Copyright © 2016 Jimmy Nordbeck. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
